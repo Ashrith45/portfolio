caca21c17c3e466890887890c003c275e88138da
Ashrith45 <ranguashrith512@gmail.com>
2026-01-03 03:45:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-const text = "Web Developer | Programmer | Problem Solver";
-let index = 0;
 
-function typeEffect() {
-    if (index < text.length) {
-        document.getElementById("typing").innerHTML += text.charAt(index);
-        index++;
-        setTimeout(typeEffect, 80);
-    }
+function changeColor() {
+    const welcomeText = document.getElementById('welcome-text');
+    
+  
+    const colors = ['#0984e3', '#00cec9', '#6c5ce7', '#fab1a0', '#e17055'];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    
+    welcomeText.style.color = randomColor;
+    console.log("Color changed to: " + randomColor);
 }
 
-typeEffect();
+
+window.onload = function() {
+    console.log("Portfolio Loaded Successfully");
+};
